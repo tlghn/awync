e988cb320317ece027754c2f18b60153f828095c
Tolgahan Albayrak <tolgahan.albayrak@live.com>
2016-11-10 05:45:37 +0200

    Release 1.2.2

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const ITER = Symbol('ITER');
 const VAL = Symbol('VAL');
 const iterator = Symbol('iterator');
 const callback = Symbol('callback');
+const captureErrors = Symbol('captureErrors');
+const releaseErrors = Symbol('releaseErrors');
 
 function awaiter(obj, target, noCache) {
     if(typeof obj === 'function'){
@@ -87,52 +89,93 @@ function isGeneratorObj(a) {
 function run(genFunc, complete) {
 
     var rootObj = genFunc();
+    var errorHandler = [];
+
     function exit(result) {
+        if(result instanceof Error){
+            if(errorHandler.length){
+                let last = errorHandler.pop();
+                return doRun(last, last.cb, last.throw(result));
+            }
+        }
         complete(result, rootObj);
     }
 
-    return (function doRun(genObj, cb) {
+    function processYieldResult(yielded, genObj, prev, next) {
+
+        if(yielded.done){
+
+            if(errorHandler.length && errorHandler[errorHandler.length - 1] === genObj){
+                errorHandler.pop();
+            }
+
+            if(genObj.cb) {
+                return genObj.cb(prev);
+            }
+
+            if(genObj === rootObj){
+                return exit(prev);
+            }
+
+            return;
+        }
+
+        if(isGeneratorObj(yielded.value)){
+            return doRun(yielded.value, next);
+        }
+
+        if(yielded.value instanceof Promise){
+            return yielded.value.then(
+                function (result) {
+                    setTimeout(next, 0, result);
+                },
+                function (err) {
+                    if(!(err instanceof Error)){
+                        err = new Error(err);
+                    }
+                    setTimeout(next, 0, err);
+                }
+            );
+        }
+
+        if(yielded.value === captureErrors){
+            if(errorHandler.indexOf(genObj) < 0){
+                errorHandler.push(genObj);
+            }
+        } else if(yielded.value === releaseErrors){
+            if(errorHandler.length && errorHandler[errorHandler.length - 1] === genObj){
+                errorHandler.pop();
+            }
+        }
+
+        next(yielded.value);
+    }
+
+    function doRun(genObj, cb, prevResult) {
+        genObj.cb = cb;
         function next(prev) {
+
             if(prev instanceof Error){
                 return exit(prev);
             }
+
             var yielded;
             try{
                 yielded = genObj.next(prev);
             }catch (err){
                 return exit(err);
             }
-            if(yielded.done){
-                if(cb) {
-                    return cb(prev);
-                }
 
-                if(genObj === rootObj){
-                    return exit(prev);
-                }
-
-                return;
-            }
-            if(isGeneratorObj(yielded.value)){
-                return doRun(yielded.value, next);
-            }
-            if(yielded.value instanceof Promise){
-                return yielded.value.then(
-                    function (result) {
-                        setTimeout(next, 0, result);
-                    },
-                    function (err) {
-                        if(!(err instanceof Error)){
-                            err = new Error(err);
-                        }
-                        setTimeout(next, 0, err);
-                    }
-                );
-            }
-            next(yielded.value);
+            processYieldResult(yielded, genObj, prev, next);
         }
-        next();
-    })(rootObj);
+        if(!prevResult){
+            next();
+        } else {
+            processYieldResult(prevResult, genObj, void 0, next);
+        }
+    }
+
+    return doRun(rootObj);
 }
 
 /**
@@ -225,11 +268,14 @@ function awync(a) {
 }
 awync.iterator = iterator;
 awync.callback = callback;
+awync.captureErrors = captureErrors;
+awync.releaseErrors = releaseErrors;
 awync.SUPPRESS_THROW = 1;
 awync.SUPPRESS_REJECT = 2;
 awync.SUPPRESS = 3;
 awync.isGeneratorFunction = isGeneratorFunc;
 awync.isGeneratorObject = isGeneratorObj;
+
 
 Object.defineProperties(awync, {
     iterator: {
@@ -245,6 +291,22 @@ Object.defineProperties(awync, {
         enumerable: false,
         writable: false,
         configurable: false
+    },
+    captureErrors: {
+        value: captureErrors,
+        __proto__: null,
+        enumerable: false,
+        writable: false,
+        configurable: false
+    },
+
+    releaseErrors: {
+        value: releaseErrors,
+        __proto__: null,
+        enumerable: false,
+        writable: false,
+        configurable: false
     }
+
 });
 module.exports = awync;
